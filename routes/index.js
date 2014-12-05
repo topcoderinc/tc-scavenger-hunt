fc15c461e1d8c81ddae861e83b5e235d3456fd01
Jeff Douglas <jeff@jeffdouglas.com>
2014-12-05 11:44:07 -0600

    added contest done for each route

--- routes/index.js
+++ routes/index.js
@@ -52,75 +52,102 @@ router.post('/start', function(req, res) {
 });
 
 router.get('/play', function(req, res) {
-  var step = currentStep(req.user);
-  // if they have an un-completed step, show it to them
-  if (step) {
-    res.json({ step: step.number, instructions: step.instructions });
-  // no un-completed step, so go find the next step
+  if (process.env.STATUS === 'open') {
+
+    var step = currentStep(req.user);
+    // if they have an un-completed step, show it to them
+    if (step) {
+      res.json({ step: step.number, instructions: step.instructions });
+    // no un-completed step, so go find the next step
+    } else {
+      // if they don't have an incomplete step, get the next one
+      var nextStep = req.user.steps.length + 1;
+      randomStep(nextStep)
+        .then(function(step) {
+          // if we found a next step, save it for them and show it
+          if (step) {
+            res.json({ step: step.number, instructions: step.instructions });
+            // save this step for the user
+            req.user.steps.push(step);
+            req.user.save();
+          } else {
+            var start = moment(req.user.startDatetime);
+            var end = moment(new Date());
+            // add in any penalties
+            end.add(req.user.hints * 2, 'minute');
+            User.update({ handle: req.user.handle}, {'$set': {
+                  'complete': true,
+                  'endDatetime': new Date(),
+                  'totalTime': Math.round((end.diff(start)/1000)/60 * 100)/100
+              }}, function(err) {
+              if (err) console.log(err);
+            });
+            res.json({ message: 'Congratulations! You have completed the scavenger hunt. Check out /leaderbaord to see your results and standings.' });
+          }
+        });
+    }
+
   } else {
-    // if they don't have an incomplete step, get the next one
-    var nextStep = req.user.steps.length + 1;
-    randomStep(nextStep)
-      .then(function(step) {
-        // if we found a next step, save it for them and show it
-        if (step) {
-          res.json({ step: step.number, instructions: step.instructions });
-          // save this step for the user
-          req.user.steps.push(step);
-          req.user.save();
-        } else {
-          var start = moment(req.user.startDatetime);
-          var end = moment(new Date());
-          // add in any penalties
-          end.add(req.user.hints * 2, 'minute');
-          User.update({ handle: req.user.handle}, {'$set': {
-                'complete': true,
-                'endDatetime': new Date(),
-                'totalTime': Math.round((end.diff(start)/1000)/60 * 100)/100
-            }}, function(err) {
-            if (err) console.log(err);
-          });
-          res.json({ message: 'Congratulations! You have completed the scavenger hunt. Check out /leaderbaord to see your results and standings.' });
-        }
-      });
+    res.json({ message: 'Sorry! The scavenger hunt is complete. Please watch our twitter feed for the next contest.' });
   }
 
 });
 
 router.post('/submit', function(req, res) {
-  var step = currentStep(req.user);
-  // prevent them from hitting the submit button twice.
-  if (step === undefined) {
-    res.json({ message: 'You have already submitted for this step. Please cURL /play again to continue.' });
-  } else {
-    if (_.indexOf(step.answer, req.body.answer.toLowerCase()) != -1) {
-      User.update({ handle: req.user.handle, 'steps.number': req.user.steps.length}, {'$set': {
-            'steps.$.complete': true,
-            'steps.$.userAnswer': req.body.answer
-        }}, function(err) {
-        if (err) console.log(err);
-      });
-      res.json({ success: true, message: 'Correct! Please cURL /play again to continue.' });
+  if (process.env.STATUS === 'open') {
+
+    var step = currentStep(req.user);
+    // prevent them from hitting the submit button twice.
+    if (step === undefined) {
+      res.json({ message: 'You have already submitted for this step. Please cURL /play again to continue.' });
     } else {
-      console.log(req.user.handle + ' answered incorrectly with: |' + req.body.answer + '|. Correct answer is:' + step.answer);
-      res.json({ success: false, message: 'Sorry. That was not the correct answer. Make sure you URL encode any text for you answer that contains spaces. You can try a /hint but it will cost you a 2 minute penalty.' });
+      if (_.indexOf(step.answer, req.body.answer.toLowerCase()) != -1) {
+        User.update({ handle: req.user.handle, 'steps.number': req.user.steps.length}, {'$set': {
+              'steps.$.complete': true,
+              'steps.$.userAnswer': req.body.answer
+          }}, function(err) {
+          if (err) console.log(err);
+        });
+        res.json({ success: true, message: 'Correct! Please cURL /play again to continue.' });
+      } else {
+        console.log(req.user.handle + ' answered incorrectly with: |' + req.body.answer + '|. Correct answer is:' + step.answer);
+        res.json({ success: false, message: 'Sorry. That was not the correct answer. Make sure you URL encode any text for you answer that contains spaces. You can try a /hint but it will cost you a 2 minute penalty.' });
+      }
     }
+
+  } else {
+    res.json({ message: 'Sorry! The scavenger hunt is complete. Please watch our twitter feed for the next contest.' });
   }
+
 });
 
 router.get('/hint', function(req, res) {
-  // increment the number of hints they requested
-  User.update({ handle: req.user.handle}, {'$inc': { 'hints': 1 }}, function(err) {
-    if (err) console.log(err);
-  });
-  var step = currentStep(req.user);
-  res.json({ step: step.number, hint: step.hint });
+  if (process.env.STATUS === 'open') {
+
+    // increment the number of hints they requested
+    User.update({ handle: req.user.handle}, {'$inc': { 'hints': 1 }}, function(err) {
+      if (err) console.log(err);
+    });
+    var step = currentStep(req.user);
+    res.json({ step: step.number, hint: step.hint });
+
+  } else {
+    res.json({ message: 'Sorry! The scavenger hunt is complete. Please watch our twitter feed for the next contest.' });
+  }
+
 });
 
 router.get('/leaderboard', function(req, res) {
-  User.find({totalTime: { $gt: 0 }}, { handle: 1, totalTime: 1} ).sort({ 'totalTime': 1 }).exec(function(err, users) {
-    res.json({ leaderboard: users });
-  });
+  if (process.env.STATUS === 'open') {
+
+    User.find({totalTime: { $gt: 0 }}, { handle: 1, totalTime: 1} ).sort({ 'totalTime': 1 }).exec(function(err, users) {
+      res.json({ leaderboard: users });
+    });
+
+  } else {
+    res.json({ message: 'Sorry! The scavenger hunt is complete. Please watch our twitter feed for the next contest.' });
+  }
+    
 });
 
 var currentStep = function(user) {
